d9ece22fcaf23b642b750f85b7e3e5d881cc2fd4
Jesus <enriquecuriel.sm@gmail.com>
2025-05-01 16:23:48 -0600

    added script to create db pokedex

---
+++ create_db.py
@@ -0,0 +1,30 @@
+import psycopg2
+from psycopg2 import sql
+# from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+# from config import config
+
+DBNAME = "postgres"
+DBUSER = "postgres"
+DBPASS = "admin"
+DBHOST = "localhost"
+
+try:
+    con = psycopg2.connect(
+        dbname = DBNAME,
+        user = DBUSER, 
+        host = DBHOST,
+        password = DBPASS
+    )
+    # con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    con.autocommit = True
+    cur = con.cursor()
+    cur.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier("pokedex")))
+    # cur.execute(sql.SQL("DROP DATABASE {}").format(sql.Identifier("pokedex")))
+    cur.close()
+    con.commit()
+    
+except Exception as e:
+    print(e)
+finally: 
+    if con is not None:
+        con.close()
